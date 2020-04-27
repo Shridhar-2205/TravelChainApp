bc84cf99c577529f82cf82d14d52db1329bcb99f
Shridhar Saurin Shah <42818678+Shridhar-2205@users.noreply.github.com>
2020-04-26 17:23:30 -0700

    Update OwnerProfileActions.js

--- frontend/src/components/dashboard/OwnerProfileActions.js
+++ frontend/src/components/dashboard/OwnerProfileActions.js
@@ -13,22 +13,22 @@ const OwnerProfileActions = () => {
 
      <p>
       <center>
-      <Link to="/add-homes" className="btn btn-light" style={{backgroundColor: "red"}}  >
-        <FaHome></FaHome> <h5>Add Home&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h5>
+      <Link to="/add-homes" className="btn btn-light" style={{backgroundColor: 'none', border:'none' }}  >
+        <FaHome></FaHome> <h4>Add Home&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4>
       </Link>
-      <Link to="/add-helicopters" className="btn btn-light" style={{backgroundColor: "green"}}>
-        <FaHelicopter></FaHelicopter> <h5>Add Helicopter&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h5>
+      <Link to="/add-helicopters" className="btn btn-light" style={{backgroundColor: 'none', border:'none'}}>
+        <FaHelicopter></FaHelicopter> <h4>Add Helicopter&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4>
       </Link>
-      <Link to="/add-farms" className="btn btn-light" style={{backgroundColor: "yellow"}}>
-        <FaTree></FaTree> <h5>Add Farm&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h5>
+      <Link to="/add-farms" className="btn btn-light" style={{backgroundColor: 'none', border:'none'}}>
+        <FaTree></FaTree> <h4>Add Farm&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4>
       </Link>
       </center>
      
       </p>
-      <Link to="/product-blockchain" className="btn btn-light" style={{ backgroundColor: 'grey'}}>
+      <Link to="/product-blockchain" className="btn btn-light" style={{ backgroundColor: 'none'}}>
                  <img src={'https://pbs.twimg.com/profile_images/1030204789978804230/ciDiP1gY_400x400.jpg'} width={5000} height={150} /><h3>Product Blockchain&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h3>
           </Link>
-      <Link to="/payment-blockchain" className="btn btn-light" style={{ backgroundColor: 'violet'}}>
+      <Link to="/payment-blockchain" className="btn btn-light" style={{ backgroundColor: 'none'}}>
                  <img src={'https://pbs.twimg.com/profile_images/1030204789978804230/ciDiP1gY_400x400.jpg'} width={5000} height={150}/><h3>Booking Blockchain&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h3>
       </Link>
      
